6295917bd5039c036e3c747f104ebd2094f8c01e
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-11-06 16:38:20 +0100

    Add process listing

---
+++ ModelProcesses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processes
+{
+    internal class ModelProcesses
+    {
+        public IEnumerable<Process> ProcessesList;
+
+        public string? ProcessId;
+        public string? ProcessName;
+        public string? ProcessTotalActiveTime;
+    }
+}
---
+++ ModelViewProcesses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Processes
+{
+    internal class ModelViewProcesses : INotifyPropertyChanged
+    {
+        private readonly ModelProcesses _model = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ModelViewProcesses()
+        {
+            _model.ProcessesList = new ObservableCollection<Process>(Process.GetProcesses()).Where(HasPriorityModifiable);
+        }
+
+        public IEnumerable<Process> ProcessesList => _model.ProcessesList;
+
+        public string? ProcessId => _model.ProcessId;
+        public string? ProcessName => _model.ProcessName;
+        public string? ProcessTotalActiveTime => _model.ProcessTotalActiveTime;
+
+        private static bool HasPriorityModifiable(Process process)
+        {
+            try
+            {
+                process.PriorityClass = process.PriorityClass;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
